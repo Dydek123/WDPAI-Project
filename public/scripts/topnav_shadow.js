4f29b4eca00c225042926af4e2496047919e8621
Dydek123 <mazurkiewicz707@gmail.com>
2021-01-16 18:51:33 +0100

    Visual changes on topnav

--- public/scripts/topnav_shadow.js
+++ public/scripts/topnav_shadow.js
@@ -6,6 +6,6 @@ function scrollFunction() {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300){
         document.getElementById("main-topnav").style.backgroundColor =  'rgba(18,35,46,1)';
     } else {
-        document.getElementById("main-topnav").style.backgroundColor =  'rgba(18,35,46,0.8)';
+        document.getElementById("main-topnav").style.background =  'linear-gradient(0deg, rgba(18,35,46,0.5) 0%, rgba(18,35,46,0.8) 52%)';
     }
 }
